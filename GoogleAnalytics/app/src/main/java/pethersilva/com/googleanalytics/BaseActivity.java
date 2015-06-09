089286329f61f54891e3832a0f1174269ad9dfc5
Pether José <pjs@cesar.org.br>
2015-06-08 23:29:33 -0300

    commeting code

--- GoogleAnalytics/app/src/main/java/pethersilva/com/googleanalytics/BaseActivity.java
+++ GoogleAnalytics/app/src/main/java/pethersilva/com/googleanalytics/BaseActivity.java
@@ -4,24 +4,39 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import com.google.android.gms.analytics.HitBuilders;
 
+/**
+ * BaseActivity is responsible to implement all behaviours that all Activities should have.
+ * Less code. Less bugs.
+ *
+ * @author Pether Silva
+ */
+
 public class BaseActivity extends ActionBarActivity {
 
+    /*
+     * Overriding onCreate method to Google Analylitics track when an Activity was created.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //Calling tracker method to send information to Google Analytics
         MyApp.tracker.setScreenName("OnCreate: " + this.getClass().getName());
         MyApp.tracker.send(new HitBuilders.EventBuilder()
-                     .setCategory("My Category")
-                     .setAction("My Action")
-                     .setLabel("My Label")
-                     .build());
+                .setCategory("My Category")
+                .setAction("My Action")
+                .setLabel("My Label")
+                .build());
     }
 
+    /*
+    * Overriding onDestroy method to Google Analylitics track when an Activity was destroyed.
+    */
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
+        //Calling tracker method to send information to Google Analytics
         MyApp.tracker.setScreenName("OnDestroy: " + this.getClass().getName());
         MyApp.tracker.send(new HitBuilders.EventBuilder()
                 .setCategory("My Category")
--- GoogleAnalytics/app/src/main/java/pethersilva/com/googleanalytics/MyApp.java
+++ GoogleAnalytics/app/src/main/java/pethersilva/com/googleanalytics/MyApp.java
@@ -4,6 +4,12 @@ import android.app.Application;
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
 
+/**
+ * MyApp is responsible to implement some Application behaviours.
+ *
+ * @author Pether Silva
+ */
+
 public class MyApp extends Application {
 
     public static GoogleAnalytics analytics;
@@ -15,7 +21,7 @@ public class MyApp extends Application {
         analytics = GoogleAnalytics.getInstance(this);
         analytics.setLocalDispatchPeriod(1800);
 
-        tracker = analytics.newTracker("UA-63802374-1");
+        tracker = analytics.newTracker("UA-63802374-1"); //Add your Google Analytics here.
         tracker.enableExceptionReporting(true);
         tracker.enableAdvertisingIdCollection(true);
         tracker.enableAutoActivityTracking(true);
